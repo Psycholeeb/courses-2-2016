c180bddb0f9dc28fdc3a13ed059b98b552a40ebb
virett27 <virett27@gmail.com>
2016-07-14 16:37:59 +0300

    HW 4 part 2

--- module4/src/main/java/com/courses/spalah/PrintMatrix.java
+++ module4/src/main/java/com/courses/spalah/PrintMatrix.java
@@ -1,5 +1,9 @@
 package com.courses.spalah;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * @author Ievgen Tararaka
  */
@@ -48,5 +52,107 @@ public class PrintMatrix {
      * @param args - размер массива
      */
     public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String input;
+        int length;
+        String[][] array;
+        try {
+            while (true) {
+                System.out.println("Введите размер массива: ");
+                input = reader.readLine();
+
+                if (input.equals("exit"))
+                    break;
+
+                length = Integer.parseInt(input);
+                array = new String[length][length];
+
+                System.out.println("Введите символ: ");
+                input = reader.readLine();
+
+                for (int i = 0; i < array.length; i++) {
+                    for (int j = 0; j < array[i].length; j++) {
+                        array[i][j] = input;
+                    }
+                }
+
+                printSquare(array);
+                System.out.println();
+
+                printTriangle1(array);
+                System.out.println();
+
+                printTriangle2(array);
+                System.out.println();
+
+                printTriangle3(array);
+                System.out.println();
+
+                printTriangle4(array);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    public static void printSquare(String[][] a){
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.print(a[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printTriangle1(String[][] a){
+        int count = 1;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < count; j++) {
+                System.out.print(a[i][j] + "\t");
+            }
+            System.out.println();
+            count++;
+        }
+    }
+
+    public static void printTriangle2(String[][] a){
+        int count = a.length;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < count; j++) {
+                System.out.print(a[i][j] + "\t");
+            }
+            System.out.println();
+            count--;
+        }
+    }
+
+    public static void printTriangle3(String[][] a){
+        int count = 1;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length - count; j++) {
+                System.out.print("\t");
+            }
+            for (int j = 0; j < count; j++) {
+                System.out.print(a[i][j]+"\t");
+            }
+            System.out.println();
+            count++;
+        }
+    }
+
+    public static void printTriangle4(String[][] a){
+        int count = a.length;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length - count; j++) {
+                System.out.print("\t");
+            }
+            for (int j = 0; j < count; j++) {
+                System.out.print(a[i][j]+"\t");
+            }
+            System.out.println();
+            count--;
+        }
+    }
+
+
 }

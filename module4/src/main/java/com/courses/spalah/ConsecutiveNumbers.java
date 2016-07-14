40ced05310bbad34351a6213a0d8c7370be83275
Tanya Magro <vasiliy260297@gmail.com>
2016-07-14 17:01:16 +0300

    home work 4

--- module4/src/main/java/com/courses/spalah/ConsecutiveNumbers.java
+++ module4/src/main/java/com/courses/spalah/ConsecutiveNumbers.java
@@ -1,5 +1,8 @@
 package com.courses.spalah;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  * @author Ievgen Tararaka
  */
@@ -7,11 +10,11 @@ public class ConsecutiveNumbers {
     /**
      * Написать программу, которая отвечает на вопрос:
      * "Содержит ли введенная строка правильную последовательность цифр?"
-     *
+     * <p>
      * Входные параметры программы:
      * 1. Строка с числами (только целые числа, которые не больше, чем int)
      * 2. Разделительный символ для этой строки. Разделительный символ в строке может быть любой, пустой в том числе.
-     *
+     * <p>
      * Например:
      * Строка - 1*2*3*6*7
      * Символ - *
@@ -19,6 +22,29 @@ public class ConsecutiveNumbers {
      *
      * @param args - аргументы коммандной строки
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String str = reader.readLine();
+        String symbol = reader.readLine();
+        if ("*".equals(symbol)) {
+            symbol = "\\*";
+        }
+        if ("+".equals(symbol)) {
+            symbol = "\\+";
+        }
+
+        String[] digits = str.split(symbol);
+
+        boolean result = false;
+        for (int i = digits.length - 1; i > 0; i--) {
+            if (Integer.parseInt(digits[i]) - Integer.parseInt(digits[i - 1]) == 1) {
+                result = true;
+            } else {
+                result = false;
+                break;
+            }
+        }
+
+        System.out.println(result);
     }
 }
--- module4/src/main/java/com/courses/spalah/PrintMatrix.java
+++ module4/src/main/java/com/courses/spalah/PrintMatrix.java
@@ -1,5 +1,8 @@
 package com.courses.spalah;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  * @author Ievgen Tararaka
  */
@@ -7,11 +10,11 @@ public class PrintMatrix {
     /**
      * Написать программу, печатает двумерный массив заданного размера
      * квадратом и 4-мя треугольниками.
-     *
+     * <p>
      * Входные параметры программы:
      * 1. Размер массива
      * 2. Символ для печати
-     *
+     * <p>
      * Пример
      * Размер массива - 5
      * Символ для печати - #
@@ -34,19 +37,131 @@ public class PrintMatrix {
      * # #
      * #
      * <p>
-     *         #
-     *       # #
-     *     # # #
-     *   # # # #
+     * #
+     * # #
+     * # # #
+     * # # # #
      * # # # # #
      * <p>
      * # # # # #
-     *   # # # #
-     *     # # #
-     *       # #
-     *         #
+     * # # # #
+     * # # #
+     * # #
+     * #
+     *
      * @param args - размер массива
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int size = Integer.parseInt(reader.readLine());
+        String symbol = reader.readLine();
+        square(size, symbol);
+        System.out.println();
+        triangle1(size, symbol);
+        System.out.println();
+        triangle2(size, symbol);
+        System.out.println();
+        triangle3(size, symbol);
+        System.out.println();
+        triangle4(size, symbol);
+
+    }
+
+    public static void square(int size, String symbol) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(symbol + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void triangle1(int size, String symbol) {
+        int n = 0;
+        int m = 0;
+        int count1 = 1;
+        int count2 = size - 1;
+        for (int i = 0; i < size; i++) {
+            while (n < count1) {
+                System.out.print(symbol + " ");
+                n++;
+            }
+            n = 0;
+            count1++;
+            while (m < count2) {
+                System.out.print(" " + " ");
+                m++;
+            }
+            m = 0;
+            count2--;
+            System.out.println();
+        }
+    }
+
+    public static void triangle2(int size, String symbol) {
+        int n = 0;
+        int m = 0;
+        int count1 = size;
+        int count2 = 1;
+        for (int i = 0; i < size; i++) {
+            while (n < count1) {
+                System.out.print(symbol + " ");
+                n++;
+            }
+            n = 0;
+            count1--;
+            while (m < count2) {
+                System.out.print(" " + " ");
+                m++;
+            }
+            m = 0;
+            count2++;
+            System.out.println();
+        }
+    }
+
+    public static void triangle3(int size, String symbol) {
+        int n = 0;
+        int m = 0;
+        int count1 = size - 1;
+        int count2 = 1;
+        for (int i = 0; i < size; i++) {
+            while (n < count1) {
+                System.out.print(" " + " ");
+                n++;
+            }
+            n = 0;
+            count1--;
+            while (m < count2) {
+                System.out.print(symbol + " ");
+                m++;
+            }
+            m = 0;
+            count2++;
+            System.out.println();
+        }
+    }
+
+    public static void triangle4(int size, String symbol) {
+        int n = 0;
+        int m = 0;
+        int count1 = 0;
+        int count2 = size;
+        for (int i = 0; i < size; i++) {
+            while (n < count1) {
+                System.out.print(" " + " ");
+                n++;
+            }
+            n = 0;
+            count1++;
+            while (m < count2) {
+                System.out.print(symbol + " ");
+                m++;
+            }
+            m = 0;
+            count2--;
+            System.out.println();
+        }
     }
 }
+

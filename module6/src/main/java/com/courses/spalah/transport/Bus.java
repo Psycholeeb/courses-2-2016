3859101d34446ff9c7d8ca03b6f05a831bf3bd44
Tanya Magro <vasiliy260297@gmail.com>
2016-07-23 21:31:46 +0300

    hw 6 changed

--- module6/src/main/java/com/courses/spalah/transport/Bus.java
+++ module6/src/main/java/com/courses/spalah/transport/Bus.java
@@ -3,7 +3,7 @@ package com.courses.spalah.transport;
 /**
  * Created by Татьяна on 23.07.2016.
  */
-public class Bus {
+public class Bus extends Car{
     private String model;
     private int numberOBus;
     private int capacityOfBus;
--- module6/src/main/java/com/courses/spalah/transport/Truck.java
+++ module6/src/main/java/com/courses/spalah/transport/Truck.java
@@ -3,7 +3,7 @@ package com.courses.spalah.transport;
 /**
  * Created by Татьяна on 23.07.2016.
  */
-public class Truck {
+public class Truck extends Car{
     private String model;
     private float volumeOfTruck;
 
